3b056b5225eaaf34ca1b94375c5fb594ebc42f9c
Juan-A <juan13herrero@gmail.com>
2019-06-17 11:38:57 +0200

    Actualización
    Corregido acceso remoto.

--- script-remoto.sh
+++ script-remoto.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-if [ ! -x /usr/local/internext/scripts-local.sh ]
-then
-mkdir -p /usr/local/internext/
-curl -O https://raw.githubusercontent.com/Juan-A/internext-script/master/scripts-local.sh
-mv scripts-local.sh /usr/local/internext/scripts-local.sh
-chmod +x /usr/local/internext/scripts-local.sh
-./usr/local/internext/scripts-local.sh
+if [ ! -x /usr/local/internext/scripts-local.sh ] && then && mkdir -p /usr/local/internext/ && curl -O https://raw.githubusercontent.com/Juan-A/internext-script/master/scripts-local.sh && mv scripts-local.sh /usr/local/internext/scripts-local.sh && chmod +x /usr/local/internext/scripts-local.sh && fi
 
--- vesta-internext.sh
+++ vesta-internext.sh
@@ -25,16 +25,36 @@ localelect(){
 	echo "---\/----------------------------"
 	echo "¡De acuerdo! - Vesta se encuentra instalado."
 	read -p "Pulsa intro para acceder al menú de opciones."
-	./funciones/scripts-local.sh
+	./usr/local/internext/scripts-local.sh
 	} 
 	
+electrem(){
+	echo ""
+read -p "¿Has ejecutado anteriormente este script en la instalación ? [Ss / Nn ]" anteriormente #Esto descarga el script de automatización en el servidor, de no estarlo, en cualquier caso, sólo debemos seleccionar NO una única vez.
+case $anteriormente in
+[sS])
+ssh root@server.internext.local -tt "/usr/local/internext/scripts-local.sh"
+;;
+[nN])
+ssh root@server.internext.local "#!/bin/bash && if [ ! -x /usr/local/internext/scripts-local.sh ] && then && mkdir -p /usr/local/internext/ && curl -O https://raw.githubusercontent.com/Juan-A/internext-script/master/scripts-local.sh && mv scripts-local.sh /usr/local/internext/scripts-local.sh && chmod +x /usr/local/internext/scripts-local.sh && fi"
+ssh root@server.internext.local -tt "/usr/local/internext/scripts-local.sh" #SI es la primera vez, he de introducir la password dos veces.
+;;
+
+*)
+read -p "Opción no válida. Pulse intro para volver al menú."
+eleclocalrem
+;;
+esac
+} 
 eleclocalrem() {
 	
 	read -p "¿Es el servidor local o remoto? [L -> Local / R -> Remoto ]": ubicacion
 case $ubicacion in
 [lL])
 localelect	
-
+;;
+[rR])
+electrem
 ;;
 [Ss])
 exit 0
